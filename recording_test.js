dd6ab90207b98b781408dcf40ad53bba24476345
Tomáš Mořkovský <56215387+morkoto1@users.noreply.github.com>
2022-04-08 13:21:05 +0200

    first test

--- recording_test.js
+++ recording_test.js
@@ -1,5 +1,10 @@
 Feature('recording');
 
 Scenario('test something', ({ I }) => {
-
+  I.amOnPage('http://stunning-test-website.tech/');
+  I.wait(1000)
+  I.click('#introNav')
+  I.wait(10000)
+  I.click('.close')
+  I.wait(10000)
 });
