b47de61fc3f10db9ffb2b0a43875cefe96314a74
Tomáš Mořkovský <56215387+morkoto1@users.noreply.github.com>
2022-04-08 13:27:20 +0200

    add semicolons

--- recording_test.js
+++ recording_test.js
@@ -2,9 +2,9 @@ Feature('recording');
 
 Scenario('test something', ({ I }) => {
   I.amOnPage('/');
-  I.wait(1000)
-  I.click('#introNav')
-  I.wait(10000)
-  I.click('.close')
-  I.wait(10000)
+  I.wait(1000);
+  I.click('#introNav');
+  I.wait(10000);
+  I.click('.close');
+  I.wait(10000);
 });
