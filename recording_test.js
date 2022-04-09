2107fbfe575b7919cf7419f061d15ba77a567f6b
morkoto <morkovskytomas@gmail.com>
2022-04-09 13:31:50 +0200

    chore: iprove record test - add move cursor

--- recording_test.js
+++ recording_test.js
@@ -3,27 +3,33 @@ Feature("recording");
 Scenario("test something", ({ I }) => {
   I.amOnPage("/");
   I.waitForText("INTRO");
+  I.moveCursorTo(".fa-diamond");
 
   // range click
+  I.wait(3);
   I.doubleClick(".fa-diamond");
   I.doubleClick(".fa-diamond");
   I.doubleClick(".fa-diamond");
 
   // open intro tab
+  I.moveCursorTo("#introNav");
   I.wait(3);
   I.click("#introNav");
   I.wait(3);
+  I.moveCursorTo(".close");
   I.click(".close");
   I.wait(3);
 
   // open about tab
   I.waitForElement("#aboutNav");
+  I.moveCursorTo("#aboutNav");
   I.click("#aboutNav");
   I.wait(3);
   I.click(".close");
 
   // open contact tab
   I.waitForElement("#contactNav");
+  I.moveCursorTo("#contactNav");
   I.click("#contactNav");
   I.wait(3);
 
@@ -34,11 +40,12 @@ Scenario("test something", ({ I }) => {
 
   // send message
   I.wait(3);
-  I.forceClick(".close");
+  I.click(".close");
   I.wait(1);
 
   // open affiliate nav
   I.waitForElement("#affiliateNav");
+  I.moveCursorTo("#affiliateNav");
   I.click("#affiliateNav");
   I.wait(3);
 
