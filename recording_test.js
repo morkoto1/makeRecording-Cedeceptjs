b8e39d1905160c3d37685abce70704354249e8f9
morkoto <morkovskytomas@gmail.com>
2022-04-09 12:24:22 +0200

    iprove test

--- recording_test.js
+++ recording_test.js
@@ -3,25 +3,45 @@ Feature("recording");
 Scenario("test something", ({ I }) => {
   I.amOnPage("/");
   I.waitForText("INTRO");
+
+  // range click
+  I.doubleClick(".fa-diamond");
+  I.doubleClick(".fa-diamond");
+  I.doubleClick(".fa-diamond");
+
+  // open intro tab
   I.wait(3);
-  I.click("#buttonIntro");
+  I.click("#introNav");
   I.wait(3);
   I.click(".close");
   I.wait(3);
 
-  I.waitForElement("#buttonAffiliate");
-  I.click("#buttonAffiliate");
+  // open about tab
+  I.waitForElement("#aboutNav");
+  I.click("#aboutNav");
   I.wait(3);
   I.click(".close");
 
-  I.waitForElement("#buttonAbout");
-  I.click("#buttonAbout");
+  // open contact tab
+  I.waitForElement("#contactNav");
+  I.click("#contactNav");
   I.wait(3);
-  I.click(".close");
 
-  I.waitForElement("#buttonContact");
-  I.click("#buttonContact");
+  // fill form
+  I.fillField("#name", "Name");
+  I.fillField("#email", "Email");
+  I.fillField("#message", "Message");
+
+  // send message
   I.wait(3);
-  I.click(".close");
+  I.forceClick(".special");
   I.wait(1);
+
+  // open affiliate nav
+  I.waitForElement("#affiliateNav");
+  I.click("#affiliateNav");
+  I.wait(3);
+
+  // click affiliate link
+  I.click("Affiliate with smartlook");
 });
