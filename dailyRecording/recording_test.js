2559d728db44790c274713052e8f4e4f5a7d4339
morkoto <morkovskytomas@gmail.com>
2022-06-16 16:04:42 +0200

    chore: clean up

--- dailyRecording/recording_test.js
+++ dailyRecording/recording_test.js
@@ -4,7 +4,7 @@ const { I, webpage } = inject();
 
 Scenario("make actions on page stunning-test-website.tech", () => {
   I.amOnPage("/");
-  I.waitForText("INTROUUUUU");
+  I.waitForText("INTRO");
   I.moveCursorTo(webpage.diamondIcon);
 
   // open popup (custom event)
